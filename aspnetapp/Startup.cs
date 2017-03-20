554f534e09bbc1fcf1b8a38e2daca48fb9705a15
Nick Ward <nickward@microsoft.com>
2017-03-21 12:27:51 +1300

    added aspnetapp

---
+++ aspnetapp/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(String.Concat("Operating system is: ", System.Runtime.InteropServices.RuntimeInformation.OSDescription));
+                await context.Response.WriteAsync(String.Concat("<br />Local IP address is: ", context.Connection.LocalIpAddress.ToString()));
+                await context.Response.WriteAsync(String.Concat("<br />Remote IP address is: ", context.Connection.RemoteIpAddress.ToString()));
+                
+            });
+        }
+    }
+}
